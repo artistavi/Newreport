06675a0bb6d0ca2959a489f0a6c04a1af59b3d11
artistavi <99407105+artistavi@users.noreply.github.com>
2022-02-10 21:03:19 +0530

    Update programgit.java

--- Demogit/src/githubpractice/programgit.java
+++ Demogit/src/githubpractice/programgit.java
@@ -3,7 +3,7 @@ package githubpractice;
 public class programgit {
 public static void main(String[] args) {
 	System.out.println("git hub");
-	System.out.println("print state")
+	System.out.println("print state");
 }
 
 }

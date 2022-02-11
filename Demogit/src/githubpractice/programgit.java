fe2de18e92c634de21a9d0f2e88645e7186f4bb1
AVANI <AVANI@DESKTOP-CI9PB33>
2022-02-11 16:48:48 +0530

    merge

--- Demogit/src/githubpractice/programgit.java
+++ Demogit/src/githubpractice/programgit.java
@@ -1,6 +1,10 @@
 package githubpractice;
 
 public class programgit {
+	public void m1()
+	{
+		System.out.println("this is my first program");
+	}
 public static void main(String[] args) {
 	System.out.println("git hub");
 	System.out.println("print state");
